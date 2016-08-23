90d9c8f21cb50572f87ea47027764906b551dee2
musedivison <patrick.mccaffrey.19@gmail.com>
2016-08-23 20:11:32 +1200

    added slider for photos and more images to main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 $( document ).ready(function (){
   $(".button-collapse").sideNav();
+  $('.carousel.carousel-slider').carousel({full_width: true, time_constant: 500});
+
+  // carousel time function
+  function runner(){
+    $('.carousel').carousel('next');
+    setTimeout(function(){
+      runner();
+    }, 6000);
+  }
+  // delayed runtime to allow for loading
+  setTimeout(function(){
+    runner();
+  }, 4000);
+
 });
