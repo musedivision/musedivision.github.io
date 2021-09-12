c207038c58d5b6c1a8a3be0faff8bfb361bc86bd
Patrick McCaffrey <patrick.mccaffrey.19@gmail.com>
2021-09-12 17:43:42 +1200

    try to deploy on pages

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:musedivision/musedivision.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
